27e5d2afde3a92f7c87e47b4066f4338a9258db2
Rachmat Fadlin <97800299+rachmat-fadlin@users.noreply.github.com>
2022-11-15 10:59:10 +0100

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const {addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler} = require("./handler");
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler
+    //options: {cors: {origin: ['*'],},},
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getBookByIdHandler
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editBookByIdHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteBookByIdHandler
+  }
+];
+
+module.exports = routes;
